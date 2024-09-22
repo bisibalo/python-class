98ad331c4eac8fd7d92c27ebe22e4de25042b596
bisibalo <olabisibalogun247@gmail.com>
2024-09-22 19:20:59 +0100

    python assignment

--- python.py
+++ python.py
@@ -83,16 +83,56 @@
 # C=int(B[0]) + int(B[1])
 # print(f'{B[0]} + {B[1]} ={C}')
 
-age=int(input("what is your age"))
-week=52 * age
-days=365 * age
-months=12 * age
+# age=int(input("what is your age"))
+# week=52 * age
+# days=365 * age
+# months=12 * age
 
-neww=52 * 90
-newd=365 * 90
-newm=12 * 90
+# neww=52 * 90
+# newd=365 * 90
+# newm=12 * 90
 
-print(f"you have {newd-days} days {neww-week} weeks and {newm-months} months left")
+# print(f"you have {newd-days} days {neww-week} weeks and {newm-months} months left")
+
+# Create a variable called "number" and assign any integer value to it.
+# Use an if-else statement to check if the number is even or odd. You can use the modulo operator (%), which returns the remainder of a division operation, 
+# to check if the number is divisible by 2. If the remainder is 0, the number is even, otherwise it is odd.
+# You can also use logical operator "and" to check if the number is divisible by 2 and greater than 10.
+# You can use logical operator "or" to check if the number is either divisible by 2 or greater than 10.
+
+# number=int(input('what is your number'))
+# if number %2 == 0:
+#     print(f'the number {number} is an even number')
+# else:
+#     if number %2 == 0 and number > 10:
+#      print(f'the number {number} is an even number')
+#     else:
+#        if number %2 == 0 or number > 10:
+#           print(f'the number {number} is an even number')
+#        else:
+#           print(f'the number{number} is an odd number')
+
+
+# Instructions :
+# you will be building a program that generates a band name based on the city you grew up in and the name of your pet.
+# To complete the project, you will need to use skills such as printing inputs, variables, new lines, string manipulation, and debugging.
+# Example Output:
+
+# Create a greeting for your program.
+# Ask the user for the city that they grew up in.
+# Ask the user for the name of a pet.
+# Combine the name of their city and pet and show them their band name.
+# Make sure the input cursor shows on a new line:
+
+name=input('what is your name')
+city=input('where did you grow up?')
+pet=input('whats the name of your pet') 
+
+print(f'welcome {name} \n your band name is {city+pet}')  
+
+
+
+
 
 
 
